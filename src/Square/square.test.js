e9ccb131b70d2b4d45cc41319a6aeb6cc6e3a6f4
Jordan Wilson <jordanwilson@Jordans-MBP.attlocal.net>
2019-09-23 23:21:09 -0400

    failing tests: rendering x when user clicks square

--- src/Square/square.test.js
+++ src/Square/square.test.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { expect } from 'chai';
 import { shallow, configure, mount } from "enzyme";
 import Square from "./square";
+
+const square = shallow(<Square/>);
 
 describe("when rendering the Square", () => {
 	it('renders without crashing', () => {
 	  shallow(<Square/>);
 	});
+	it("Square value should turn to 'X' during inital click", () => {
+		square.find('button').simulate('click');
+		expect(square.props().value).to.equal('X');
+  });
 });
